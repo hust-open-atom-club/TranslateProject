97b4df53b45cc848a416b11dc71059e022f74bd9
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2024-03-04 22:41:30 +0800

    chore: use system-ui font.

--- pages/tailwind.config.cjs
+++ pages/tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["system-ui", "IBM Plex Mono", "monospace"],
       },
 
       typography: {
